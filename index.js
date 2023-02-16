a34f0ac04a73137b205adf9d0b223f29ac972976
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-02-16 23:30:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -135,3 +135,23 @@ console.log(fruits);
 
 /* --------------------------------- TASK 7 --------------------------------- */
 
+/* Объяви переменную fruitsArrayLength и присвой ей длину массива fruits используя свойство length.
+
+Объявлена переменная fruitsArrayLength
+Значение переменной fruitsArrayLength это число 4 */
+
+
+/* 
+
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const fruitsArrayLength = fruits.length;
+
+console.log(fruits);
+
+*/
+
+
+
+/* --------------------------------- TASK 8 --------------------------------- */
