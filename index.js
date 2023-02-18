603767a64140a943d8c76a033e176561131d8989
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-02-18 23:37:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,17 @@ console.log(checkAge(38));
 
 /* 
 
+function checkPassword(password) {
+  const ADMIN_PASSWORD = "jqueryismyjam";
+  // Change code below this line
+
+  if (password === ADMIN_PASSWORD) {
+    return "Welcome!";
+  } 
+
+  return "Access denied, wrong password!";
+  // Change code above this line
+}
 
 
 */
