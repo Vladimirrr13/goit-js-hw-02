9a39ef60b81eb92e70df526b5bd0448a90277f18
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-02-20 23:28:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -367,3 +367,40 @@ console.log(slugify("Ten secrets of JavaScript"));
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
 
 */
+
+
+
+/* --------------------------------- TASK 14 --------------------------------- */
+
+/* Дополни код так, чтобы переменные содержали частичные копии исходного массива fruits.
+
+firstTwoEls - массив из первых двух элементов
+nonExtremeEls - массив из всех элементов кроме первого и последнего
+lastThreeEls - массив из трёх последних элементов
+Объявлена переменная fruits
+Значение переменной fruits это массив ["apple", "plum", "pear", "orange", "banana"]
+Объявлена переменная firstTwoEls
+Значение переменной firstTwoEls это массив ["apple", "plum"]
+Объявлена переменная nonExtremeEls
+Значение переменной nonExtremeEls это массив ["plum", "pear", "orange"]
+Объявлена переменная lastThreeEls
+Значение переменной lastThreeEls это массив ["pear", "orange", "banana"]
+Переменной lastThreeEls присвоена копия части массива fruits после применения метода slice с правильными аргументами */
+
+
+/* 
+
+const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// Change code below this line
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(1, 4);
+const lastThreeEls = fruits.slice(2);
+
+
+console.log(fruits);
+console.log(firstTwoEls);
+console.log(nonExtremeEls);
+console.log(lastThreeEls);
+
+*/
