be0ea65337da0e269e76d559d903754f8d19a83c
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-02-27 23:48:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -467,3 +467,46 @@ for (let i = start; i <= end; i += 1) { // Change this line
 }
 
 */
+
+
+/* --------------------------------- TASK 18 --------------------------------- */
+
+/* Напиши функцию calculateTotal(number), которая принимает целое число (параметр number) и возвращает сумму всех целых чисел от единицы и до этого числа. Например, если number равно 3, то сумма это 1 + 2 + 3, то есть 6.
+
+Объявлена функция calculateTotal(number)
+Вызов функции calculateTotal(1) возвращает 1
+Вызов функции calculateTotal(3) возвращает 6
+Вызов функции calculateTotal(7) возвращает 28
+Вызов функции calculateTotal(18) возвращает 171
+Вызов функции calculateTotal(24) возвращает 300
+Вызов функции calculateTotal() со случайным числом возвращает правильное значение */
+
+
+/* 
+
+function calculateTotal(number) {
+ // Change code below this line
+
+  let total = 0;
+
+  for (i = 0; i <= number; i += 1) {
+    total += i;
+  }
+
+  return total; 
+
+  // Change code above this line
+}
+
+console.log(calculateTotal(1));
+console.log(calculateTotal(3));
+console.log(calculateTotal(7));
+console.log(calculateTotal(18));
+console.log(calculateTotal(24));
+console.log(calculateTotal(32));
+
+*/
+
+
+
+
