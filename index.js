3362b205975acace0d5b9c069ff0b0794abcff65
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-05 23:53:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -566,3 +566,16 @@ console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
 
 */
 
+
+
+
+
+/* --------------------------------- TASK 21 --------------------------------- */
+
+/* Напиши функцию findLongestWord(string) которая принимает произвольную строку состоящую только из слов разделённых пробелом (параметр string) и возвращает самое длинное слово в этой строке.
+
+Объявлена функция findLongestWord(string)
+Вызов функции findLongestWord("The quick brown fox jumped over the lazy dog") возвращает jumped
+Вызов функции findLongestWord("Google do a roll") возвращает Google
+Вызов функции findLongestWord("May the force be with you") возвращает force
+Вызов функции findLongestWord() со случайной строкой возвращает правильное значение */
