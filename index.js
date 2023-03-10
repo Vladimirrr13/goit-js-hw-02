6e2290387c9691c7c577b7f3f9dd9a4f42279a57
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-10 23:02:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -732,3 +732,27 @@ console.log(checkFruit("apple"));
 Вызов getCommonElements([1, 2, 3], [10, 20, 30]) возвращает []
 Вызов функции getCommonElements() со случайными двумя массивами возвращает правильный массив
 В цикле for использовались методы includes и push */
+
+
+/* function getCommonElements(array1, array2) {
+  const commonElements = [];
+
+  for (let i = 0; i < array1.length; i+=1) {
+    if (array2.includes(array1[i])) {
+      commonElements.push(array1[i]);
+    }
+  }
+
+  return commonElements;
+}
+
+console.log(getCommonElements([1, 2, 3], [2, 4]));
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+console.log(getCommonElements([1, 2, 3], [10, 20, 30])); */
+
+
+
+
+
