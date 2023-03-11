335ddb728b09abf7bc834ecf12b68b0dd5847526
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-11 23:05:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -785,3 +785,43 @@ console.log(calculateTotalPrice([]));
 
 
 
+
+/* --------------------------------- TASK 27 --------------------------------- */
+
+/* 
+Выполни рефакторинг функции filterArray(numbers, value) заменив цикл for на for...of.
+
+Объявлена функция filterArray(numbers, value)
+Вызов функции filterArray([1, 2, 3, 4, 5], 3) возвращает [4, 5]
+Вызов функции filterArray([1, 2, 3, 4, 5], 4) возвращает [5]
+Вызов функции filterArray([1, 2, 3, 4, 5], 5) возвращает []
+Вызов функции filterArray([12, 24, 8, 41, 76], 38) возвращает [41, 76]
+Вызов функции filterArray([12, 24, 8, 41, 76], 20) возвращает [24, 41, 76]
+Вызов функции filterArray() со случайным массивом и числом возвращает правильный массив
+Функция filterArray() использует цикл for..of 
+*/
+
+
+/* function filterArray(numbers, value) {
+  // Change code below this line
+  const filteredNumbers = [];
+
+  for (let number of numbers) {
+
+
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
+  // Change code above this line
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20)); */
+
+
