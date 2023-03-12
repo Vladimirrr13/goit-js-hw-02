3eb8eb083ae498c92e7dba4f5a2d515a45c610a3
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-03-12 22:19:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -964,3 +964,40 @@ console.log(findNumber());
 */
 
 
+
+/* --------------------------------- TASK 32 --------------------------------- */
+
+/* Напиши функцию includes(array, value), которая делает тоже самое, что и метод массива массив.includes(значение) - проверяет, есть ли в массиве array значение value, возвращая true если есть и false в противном случае.
+
+При выполнении этой задачи в теле функции includes() нельзя использовать метод массив.includes(значение).
+
+Объявлена функция includes(array, value)
+Вызов includes([1, 2, 3, 4, 5], 3) возвращает true
+Вызов includes([1, 2, 3, 4, 5], 17) возвращает false
+Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter") возвращает true
+Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus") возвращает false
+Вызов includes(["apple", "plum", "pear", "orange"], "plum") возвращает true
+Вызов includes(["apple", "plum", "pear", "orange"], "kiwi") возвращает false
+Вызов includes() для случайного массива со случайным value возвращает верный boolean
+В функции includes используется for, return, но не метод массива includes */
+
+
+/* 
+
+function includes(array, value) {
+  for (let i = 0; i < array.length; i+=1) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(includes([1, 2, 3, 4, 5], 3));
+console.log(includes([1, 2, 3, 4, 5], 17));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
+
+*/
